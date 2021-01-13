f5de3bcc4674c9dc0c49927312e077b9b00f66f7
Unknown <prapapornyingyongkit@gmail.com>
2021-01-13 12:30:59 +0700

    login,logout ด้วย gmail + โชว์ข้อมูล user

--- app/src/main/java/com/seniorproject/test/MainActivity.kt
+++ app/src/main/java/com/seniorproject/test/MainActivity.kt
@@ -1,26 +1,47 @@
 package com.seniorproject.test
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+        //If user is not authenticated, send him to SignInActivity to authenticate first
+        //else send him to DashboardActivity
+        Handler().postDelayed({
+            if (user != null){
+                val dashboardIntent = Intent(this,Navigationbar::class.java)
+                startActivity(dashboardIntent)
+                finish()
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_account, R.id.navigation_bookmark, R.id.navigation_history, R.id.navigation_location))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+            } else {
+                val signInIntent = Intent(this,SignIn::class.java)
+                startActivity(signInIntent)
+                finish()
+            }
+
+        },2000)
+//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+//
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.navigation_home, R.id.navigation_account, R.id.navigation_bookmark, R.id.navigation_history, R.id.navigation_location))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/seniorproject/test/ui/account/AccountFragment.kt
+++ app/src/main/java/com/seniorproject/test/ui/account/AccountFragment.kt
@@ -1,30 +1,79 @@
 package com.seniorproject.test.ui.account
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.larswerkman.lobsterpicker.sliders.LobsterShadeSlider
+import com.mikhaellopez.circularimageview.CircularImageView
 import com.seniorproject.test.R
+import com.seniorproject.test.SignIn
 
 class AccountFragment : Fragment() {
     private lateinit var accountViewModel: AccountViewModel
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        accountViewModel =
-                ViewModelProvider(this).get(AccountViewModel::class.java)
+//        accountViewModel =
+//                ViewModelProvider(this).get(AccountViewModel::class.java)
+//        val root = inflater.inflate(R.layout.fragment_account, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_account)
+//        accountViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
         val root = inflater.inflate(R.layout.fragment_account, container, false)
-        val textView: TextView = root.findViewById(R.id.text_account)
-        accountViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val name: TextView = root.findViewById(R.id.name_txt)
+        val mail: TextView = root.findViewById(R.id.email_txt)
+        name.text = currentUser?.displayName
+        mail.text = currentUser?.email
+        val circularImageView = root.findViewById<CircularImageView>(R.id.circularImageView)
+        Glide.with(this).load(currentUser?.photoUrl).circleCrop().into(circularImageView)
+        circularImageView.apply {
+            // Set Color
+            circleColor = Color.WHITE
+            // or with gradient
+            circleColorStart = Color.WHITE
+            circleColorEnd = Color.WHITE
+            circleColorDirection = CircularImageView.GradientDirection.TOP_TO_BOTTOM
+
+            // Set Border
+            borderWidth = 10f
+            borderColor = Color.WHITE
+            // or with gradient
+            borderColorStart = Color.WHITE
+            borderColorEnd = Color.WHITE
+            borderColorDirection = CircularImageView.GradientDirection.TOP_TO_BOTTOM
+
+            // Add Shadow with default param
+            shadowEnable = true
+            // or with custom param
+            shadowRadius = 7f
+            shadowColor = Color.WHITE
+            shadowGravity = CircularImageView.ShadowGravity.CENTER
+        }
+        val button: Button = root.findViewById(R.id.sign_out_btn)
+        button.setOnClickListener {
+            mAuth.signOut()
+            val intent = Intent(getActivity(), SignIn::class.java)
+            startActivity(intent)
+        }
+
         return root
     }
 }
